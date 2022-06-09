a9260bcff9a6780709ce2bade2642588e7790ae5
Brichka-Dv <107205384+Brichka-Dv@users.noreply.github.com>
2022-06-09 17:48:20 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,13 @@
+Config = {}
+Config.StartingApartment = true -- Enable/disable starting apartments (make sure to set default spawn coords)
+Config.Interior = vector3(-814.89, 181.95, 76.85) -- Interior to load where characters are previewed
+Config.DefaultSpawn = vector3(-1035.71, -2731.87, 12.86) -- Default spawn coords if you have start apartments disabled
+Config.PedCoords = vector4(-804.14, 172.13, 73.34, 298.76) -- Create preview ped at these coordinates
+Config.HiddenCoords = vector4(-812.23, 182.54, 76.74, 156.5) -- Hides your actual ped while you are in selection
+Config.CamCoords = vector4(-801.31, 174.45, 72.83, 136.12) -- Camera coordinates for character preview screen
+Config.EnableDeleteButton = true -- Define if the player can delete the character or not
+
+Config.DefaultNumberOfCharacters = 2 -- Define maximum amount of default characters (maximum 5 characters defined by default)
+Config.PlayersNumberOfCharacters = { -- Define maximum amount of player characters by rockstar license (you can find this license in your server's database in the player table)
+    { license = "license:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", numberOfChars = 2 },
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,29 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'QB-Multicharacter'
+version '1.0.0'
+
+shared_script 'config.lua'
+client_script 'client/main.lua'
+server_scripts  {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/reset.css',
+    'html/profanity.js',
+    'html/script.js'
+}
+
+dependencies {
+    'qb-core',
+    'qb-spawn'
+}
+
+lua54 'yes'
